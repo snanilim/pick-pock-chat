2f84281cae714dd28853394123b3037685ee2fc3
aiaoio <snanilim@gmail.com>
2018-04-25 00:03:42 +0600

    ep 03 Done

--- app/src/components/app.js
+++ app/src/components/app.js
@@ -1,12 +1,23 @@
 import React, {Component} from 'react'
-
+import Store from '../store'
 import Messenger from './messenger'
 
 export default class App extends Component{
 
-	render(){
-		return  <div className="app-wrapper">
-				    <Messenger />
-			    </div>
+	constructor(props){
+		super(props);
+
+		this.state = {
+
+			store: new Store(this),
+		}
 	}
-} 
+
+	render(){
+
+		const {store} = this.state;
+		return <div className="app-wrapper">
+				<Messenger store={store} />
+			</div>
+	}
+}
